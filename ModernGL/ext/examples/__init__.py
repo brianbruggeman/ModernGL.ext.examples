07c63da015b9136762c921a0ef40016d60332759
Szabolcs Dombi <cprogrammer1994@gmail.com>
2017-10-22 00:59:25 +0300

    bump to version 1.2.0

--- ModernGL/ext/examples/__init__.py
+++ ModernGL/ext/examples/__init__.py
@@ -6,7 +6,7 @@
 
 from .window import run_example
 
-__version__ = '0.9.1'
+__version__ = '1.2.0'
 
 __all__ = [
     'run_example'
--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ if 'NO_PILLOW' not in os.environ:
 
 setup(
     name='ModernGL.ext.examples',
-    version='1.1.0',
+    version='1.2.0',
     author='Szabolcs Dombi',
     author_email='cprogrammer1994@gmail.com',
     description='ModernGL extension',
